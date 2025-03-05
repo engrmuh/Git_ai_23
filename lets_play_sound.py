bcc26f55f57b8f820090f413fddb894fa861dc5e
muh <umer.haroon@nu.edu.pk>
2025-03-05 16:43:34 +0500

    try again

--- lets_play_sound.py
+++ lets_play_sound.py
@@ -3,3 +3,6 @@ from playsound import playsound
 mp3_file_path = "C:\\Users\\Umer Haroon\\Music\\Y2Mate.is - New MedleyАрабы на русском. Allahu Allah,Hasbi rabbi, Nashidul Islam Mohamed Tarek Mohamed Youssef-twHqsRTlmqw-64k-1654121478710.mp3"
 
 playsound(mp3_file_path)
+
+# expectation
+# goals
